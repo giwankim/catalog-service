768232c7395f6f4d4c43fa11ba31665c240e34b4
Giwan Kim <giwan.kim@gmail.com>
2025-07-29 12:02:08 +0900

    test: integrate end-to-end tests for BookController
    - Added e2e tests for CRUD operations in `CatalogServiceApplicationTests` using `WebTestClient`.
    - Verified get, create, update, and delete operations.
    - Added dependency for `spring-boot-starter-webflux` in `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,11 +1,94 @@
 package com.polarbookshop.catalogservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class CatalogServiceApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+record CatalogServiceApplicationTests(WebTestClient testClient, BookRepository bookRepository) {
+
+  @BeforeEach
+  void setUp() {
+    bookRepository.findAll().forEach(book -> bookRepository.deleteByIsbn(book.isbn()));
+  }
 
   @Test
-  void contextLoads() {}
+  void whenGetRequestWithIdThenBookReturned() {
+    String isbn = "1231231230";
+    Book book = new Book(isbn, "Title", "Author", 9.90);
+    testClient.post().uri("/books").bodyValue(book).exchange().expectStatus().isCreated();
+
+    testClient
+        .get()
+        .uri("/books/{isbn}", isbn)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(Book.class)
+        .value(
+            actualBook -> {
+              assertThat(actualBook).isNotNull();
+              assertThat(actualBook.isbn()).isEqualTo(isbn);
+            });
+  }
+
+  @Test
+  void whenPostRequestThenBookCreated() {
+    Book book = new Book("1231231230", "Title", "Author", 9.90);
+
+    testClient
+        .post()
+        .uri("/books")
+        .bodyValue(book)
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .expectBody(Book.class)
+        .value(
+            actualBook -> {
+              assertThat(actualBook).isNotNull();
+              assertThat(actualBook.isbn()).isEqualTo(book.isbn());
+            });
+  }
+
+  @Test
+  void whenPutRequestThenBookUpdated() {
+    String isbn = "1231231230";
+    Book book = new Book(isbn, "Title", "Author", 9.90);
+    testClient.post().uri("/books").bodyValue(book).exchange().expectStatus().isCreated();
+
+    Book updatedBook = new Book(isbn, "Updated Title", "Updated Author", 7.95);
+
+    testClient
+        .put()
+        .uri("/books/{isbn}", isbn)
+        .bodyValue(updatedBook)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(Book.class)
+        .value(
+            actualBook -> {
+              assertThat(actualBook).isNotNull();
+              assertThat(actualBook.title()).isEqualTo(updatedBook.title());
+              assertThat(actualBook.author()).isEqualTo(updatedBook.author());
+              assertThat(actualBook.price()).isEqualTo(updatedBook.price());
+            });
+  }
+
+  @Test
+  void whenDeleteRequestThenBookDeleted() {
+    String isbn = "1231231230";
+    Book book = new Book(isbn, "Title", "Author", 9.90);
+    testClient.post().uri("/books").bodyValue(book).exchange().expectStatus().isCreated();
+
+    testClient.delete().uri("/books/{isbn}", isbn).exchange().expectStatus().isNoContent();
+
+    testClient.get().uri("/books/{isbn}", isbn).exchange().expectStatus().isNotFound();
+  }
 }
